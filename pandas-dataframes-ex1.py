de8ae24093304b5f59ba4462b7166b2323068c68
LReyna <leo.reyna@gmail.com>
2023-01-28 13:28:04 -0500

    new files

---
+++ pandas-dataframes-ex1.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+
+os.system('cls')
+df = pd.DataFrame()
+print(df)
+
+
---
+++ pandas-dataframes-ex2.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+
+os.system('cls')
+
+
+import pandas as pd
+data = [['Alex',10],['Bob',12],['Clarke',13]]
+df = pd.DataFrame(data,columns=['Name','Age'])
+print(df)
